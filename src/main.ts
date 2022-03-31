eaa47e7d9238180eb8c428e7c42e3a7a7196ffe3
Lucas Dower <lucasjdower@gmail.com>
2022-03-31 20:37:02 +0100

    Added version to window title bar

--- src/main.ts
+++ src/main.ts
@@ -43,8 +43,7 @@ function createWindow() {
         },
     });
     // mainWindow.removeMenu();
-
-
+    
     // Load index.html
     mainWindow.loadURL(url.format({
         pathname: path.join(BASE_DIR, './index.html'),
@@ -52,6 +51,15 @@ function createWindow() {
         slashes: true,
     }));
 
+    const baseTitle = 'ObjToSchematic – Convert 3D models into Minecraft builds';
+    try {
+        const branchName = require('child_process').execSync('git rev-parse --abbrev-ref HEAD');
+        const commitHash = require('child_process').execSync('git rev-parse --short HEAD');
+        mainWindow.setTitle(`${baseTitle} (git//${branchName}++${commitHash})`);
+    } catch (e: any) {
+        mainWindow.setTitle(`${baseTitle} (release//v0.5.0)`);
+    }
+    
     // Open the DevTools.
     // mainWindow.webContents.openDevTools();
 
