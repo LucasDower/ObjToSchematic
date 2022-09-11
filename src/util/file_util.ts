6f3fcd1ab763377048694e2b45c4a0d60ccf6300
Lucas Dower <lucasjdower@gmail.com>
2022-09-11 20:32:17 +0100

    Fix for running test when out dir doesn't exist

--- src/util/file_util.ts
+++ src/util/file_util.ts
@@ -4,4 +4,10 @@ export namespace FileUtil {
     export function fileExists(absolutePath: string) {
         return fs.existsSync(absolutePath);
     }
+
+    export function mkdirSyncIfNotExist(path: fs.PathLike) {
+        if (!fs.existsSync(path)) {
+            fs.mkdirSync(path);
+        }
+    }
 }
