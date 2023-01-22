285bbe3dd40e72196919e7948d3e283cd54d3366
Lucas Dower <lucasjdower@gmail.com>
2023-01-22 15:45:42 +0000

    Refactored special block arrays from BlockMesh into `AppRuntimeConstants` * Special block arrays are now only loaded once instead of being loaded each time a new block mesh is created which is expensive

--- src/block_mesh.ts
+++ src/block_mesh.ts
@@ -5,6 +5,7 @@ import { AtlasPalette, EFaceVisibility } from './block_assigner';
 import { BlockInfo } from './block_atlas';
 import { ChunkedBufferGenerator, TBlockMeshBufferDescription } from './buffer';
 import { RGBA_255, RGBAUtil } from './colour';
+import { AppRuntimeConstants } from './constants';
 import { Ditherer } from './dither';
 import { BlockMeshLighting } from './lighting';
 import { Palette } from './palette';
@@ -13,7 +14,6 @@ import { StatusHandler } from './status';
 import { ColourSpace, TOptional } from './util';
 import { AppError, ASSERT } from './util/error_util';
 import { LOGF } from './util/log_util';
-import { AppPaths, PathUtil } from './util/path_util';
 import { Vector3 } from './vector';
 import { Voxel, VoxelMesh } from './voxel_mesh';
 import { AssignParams } from './worker_types';
@@ -43,10 +43,6 @@ export class BlockMesh {
     private _blocksUsed: Set<string>;
     private _blocks: Block[];
     private _voxelMesh: VoxelMesh;
-    private _fallableBlocks: string[];
-    private _transparentBlocks: string[];
-    private _grassLikeBlocks: string[];
-    private _emissiveBlocks: string[];
     private _atlas: Atlas;
     private _lighting: BlockMeshLighting;
 
@@ -72,20 +68,6 @@ export class BlockMesh {
         this._voxelMesh = voxelMesh;
         this._atlas = Atlas.getVanillaAtlas()!;
         this._lighting = new BlockMeshLighting(this);
-        //this._lighting = new Map<string, number>();
-        //this._recreateBuffer = true;
-
-        const fallableBlocksString = fs.readFileSync(PathUtil.join(AppPaths.Get.resources, 'fallable_blocks.json'), 'utf-8');
-        this._fallableBlocks = JSON.parse(fallableBlocksString).fallable_blocks;
-
-        const transparentlocksString = fs.readFileSync(PathUtil.join(AppPaths.Get.resources, 'transparent_blocks.json'), 'utf-8');
-        this._transparentBlocks = JSON.parse(transparentlocksString).transparent_blocks;
-
-        const emissivelocksString = fs.readFileSync(PathUtil.join(AppPaths.Get.resources, 'emissive_blocks.json'), 'utf-8');
-        this._emissiveBlocks = JSON.parse(emissivelocksString).emissive_blocks;
-
-        const grassLikeBlocksString = fs.readFileSync(PathUtil.join(AppPaths.Get.resources, 'grass_like_blocks.json'), 'utf-8');
-        this._grassLikeBlocks = JSON.parse(grassLikeBlocksString).grass_like_blocks;
     }
 
     /**
@@ -125,7 +107,7 @@ export class BlockMesh {
 
         const atlasPalette = new AtlasPalette(atlas, palette);
         const allBlockCollection = atlasPalette.createBlockCollection([]);
-        const nonFallableBlockCollection = atlasPalette.createBlockCollection(this._fallableBlocks);
+        const nonFallableBlockCollection = atlasPalette.createBlockCollection(AppRuntimeConstants.Get.FALLABLE_BLOCKS);
         const grassLikeBlocksBuffer: GrassLikeBlock[] = [];
 
         let countFalling = 0;
@@ -144,7 +126,7 @@ export class BlockMesh {
 
             // Check that this block meets the fallable behaviour, we may need
             // to choose a different block if the current one doesn't meet the requirements
-            const isBlockFallable = this._fallableBlocks.includes(block.name);
+            const isBlockFallable = AppRuntimeConstants.Get.FALLABLE_BLOCKS.includes(block.name);
             const isBlockSupported = this._voxelMesh.isVoxelAt(Vector3.add(voxel.position, new Vector3(0, -1, 0)));
 
             if (isBlockFallable && !isBlockSupported) {
@@ -159,7 +141,7 @@ export class BlockMesh {
                 block = atlasPalette.getBlock(voxelColour, nonFallableBlockCollection, faceVisibility, blockMeshParams.errorWeight);
             }
 
-            if (this._grassLikeBlocks.includes(block.name)) {
+            if (AppRuntimeConstants.Get.GRASS_LIKE_BLOCKS.includes(block.name)) {
                 grassLikeBlocksBuffer.push({
                     id: this._blocks.length,
                     voxelColour: voxelColour,
@@ -176,13 +158,13 @@ export class BlockMesh {
         }
 
         if (grassLikeBlocksBuffer.length > 0) {
-            const nonGrassLikeBlockCollection = atlasPalette.createBlockCollection(this._grassLikeBlocks);
+            const nonGrassLikeBlockCollection = atlasPalette.createBlockCollection(AppRuntimeConstants.Get.GRASS_LIKE_BLOCKS);
             for (let index=0; index < grassLikeBlocksBuffer.length; index++) {
                 ProgressManager.Get.progress(taskHandle, index / grassLikeBlocksBuffer.length);
                 const examined = grassLikeBlocksBuffer[index];
                 const examinedBlock = this._blocks[examined.id];
                 const topBlockId = this._blocks.findIndex((b) => b.voxel.position.equals(Vector3.add(examinedBlock.voxel.position, new Vector3(0, 1, 0))));
-                if (topBlockId > -1 && !this._transparentBlocks.includes(this._blocks[topBlockId].blockInfo.name)) {
+                if (topBlockId > -1 && !AppRuntimeConstants.Get.TRANSPARENT_BLOCKS.includes(this._blocks[topBlockId].blockInfo.name)) {
                     const block = atlasPalette.getBlock(examined.voxelColour, nonGrassLikeBlockCollection, examined.faceVisibility, examined.errWeight);
                     examinedBlock.blockInfo = block;
                     this._blocks[examined.id] = examinedBlock;
@@ -215,7 +197,7 @@ export class BlockMesh {
         ASSERT(voxel !== undefined, 'Missing voxel');
         const minError = Infinity;
         let bestBlock: TAtlasBlock | undefined;
-        this._emissiveBlocks.forEach((emissiveBlockName) => {
+        AppRuntimeConstants.Get.EMISSIVE_BLOCKS.forEach((emissiveBlockName) => {
             const emissiveBlockData = this._atlas.getBlocks().get(emissiveBlockName);
             if (emissiveBlockData) {
                 const error = RGBAUtil.squaredDistance(emissiveBlockData.colour, voxel.colour);
@@ -263,11 +245,11 @@ export class BlockMesh {
     }
 
     public isEmissiveBlock(block: Block) {
-        return this._emissiveBlocks.includes(block.blockInfo.name);
+        return AppRuntimeConstants.Get.EMISSIVE_BLOCKS.includes(block.blockInfo.name);
     }
 
     public isTransparentBlock(block: Block) {
-        return this._transparentBlocks.includes(block.blockInfo.name);
+        return AppRuntimeConstants.Get.TRANSPARENT_BLOCKS.includes(block.blockInfo.name);
     }
 
     /*
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,9 @@
+
+import fs from 'fs';
+
+import { AppTypes } from './util';
+import { AppPaths, PathUtil } from './util/path_util';
+
 export namespace AppConstants {
     export const FACES_PER_VOXEL = 6;
     export const VERTICES_PER_FACE = 4;
@@ -25,4 +31,31 @@ export namespace AppConstants {
     }
 
     export const DATA_VERSION = 3105; // 1.19
+}
+
+export class AppRuntimeConstants {
+    /* Singleton */
+    private static _instance: AppRuntimeConstants;
+    public static get Get() {
+        return this._instance || (this._instance = new this());
+    }
+
+    public readonly FALLABLE_BLOCKS: AppTypes.TNamespacedBlockName[];
+    public readonly TRANSPARENT_BLOCKS: AppTypes.TNamespacedBlockName[];
+    public readonly GRASS_LIKE_BLOCKS: AppTypes.TNamespacedBlockName[];
+    public readonly EMISSIVE_BLOCKS: AppTypes.TNamespacedBlockName[];
+
+    private constructor() {
+        const fallableBlocksString = fs.readFileSync(PathUtil.join(AppPaths.Get.resources, 'fallable_blocks.json'), 'utf-8');
+        this.FALLABLE_BLOCKS = JSON.parse(fallableBlocksString).fallable_blocks;
+
+        const transparentBlocksString = fs.readFileSync(PathUtil.join(AppPaths.Get.resources, 'transparent_blocks.json'), 'utf-8');
+        this.TRANSPARENT_BLOCKS = JSON.parse(transparentBlocksString).transparent_blocks;
+
+        const emissiveBlocksString = fs.readFileSync(PathUtil.join(AppPaths.Get.resources, 'emissive_blocks.json'), 'utf-8');
+        this.GRASS_LIKE_BLOCKS = JSON.parse(emissiveBlocksString).emissive_blocks;
+
+        const grassLikeBlocksString = fs.readFileSync(PathUtil.join(AppPaths.Get.resources, 'grass_like_blocks.json'), 'utf-8');
+        this.EMISSIVE_BLOCKS = JSON.parse(grassLikeBlocksString).grass_like_blocks;
+    }
 }
