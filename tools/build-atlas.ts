9bb3e4fe9f08f9aca72e5c7031977ec97ee507be
Lucas Dower <lucasjdower@gmail.com>
2023-09-14 01:20:59 +0100

    Removed `misc.ts`, moved functions into `build-atlas.ts`

--- tools/build-atlas.ts
+++ tools/build-atlas.ts
@@ -2,9 +2,55 @@ import { program } from 'commander';
 import fs from 'fs';
 import path from 'path';
 import sharp from 'sharp';
-import { getAverageColour, getStandardDeviation } from './misc';
+
 import { ASSERT } from '../src/runtime/util/error_util';
 import { RGBAUtil } from '../src/runtime/colour';
+import { RGBA } from '../src/runtime/colour';
+
+export function getAverageColour(image: Uint8ClampedArray): RGBA {
+    let r = 0;
+    let g = 0;
+    let b = 0;
+    let a = 0;
+    let weight = 0;
+    for (let x = 0; x < 16; ++x) {
+        for (let y = 0; y < 16; ++y) {
+            const index = 4 * (16 * y + x);
+            const rgba = image.slice(index, index + 4);
+            const alpha = rgba[3] / 255;
+            r += (rgba[0] / 255) * alpha;
+            g += (rgba[1] / 255) * alpha;
+            b += (rgba[2] / 255) * alpha;
+            a += alpha;
+            weight += alpha;
+        }
+    }
+    const numPixels = 16 * 16;
+    return {
+        r: r / weight,
+        g: g / weight,
+        b: b / weight,
+        a: a / numPixels,
+    };
+}
+
+export function getStandardDeviation(image: Uint8ClampedArray, average: RGBA): number {
+    let squaredDist = 0.0;
+    let weight = 0.0;
+    for (let x = 0; x < 16; ++x) {
+        for (let y = 0; y < 16; ++y) {
+            const index = 4 * (16 * y + x);
+            const rgba = image.slice(index, index + 4);
+            const alpha = rgba[3] / 255;
+            weight += alpha;
+            const r = (rgba[0] / 255) * alpha;
+            const g = (rgba[1] / 255) * alpha;
+            const b = (rgba[2] / 255) * alpha;
+            squaredDist += Math.pow(r - average.r, 2) + Math.pow(g - average.g, 2) + Math.pow(b - average.b, 2);
+        }
+    }
+    return Math.sqrt(squaredDist / weight);
+}
 
 program
     .argument('<textures_directory>', 'The directory to load the blocks texture files from (assets/minecraft/textures/block)')
