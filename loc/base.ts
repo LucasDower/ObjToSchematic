913f59f6f06dd7913632f21d938a5118d2aed455
dirtTW <a0932636771@gmail.com>
2024-08-26 18:32:01 +0700

    Add Traditional Chinese (zh_TW.ts)
    Add Traditional Chinese localization

--- loc/base.ts
+++ loc/base.ts
@@ -4,6 +4,7 @@ import { en_US } from './en_US';
 import { fr_FR } from './fr_FR';
 import { ru_RU } from './ru_RU';
 import { zh_CN } from './zh_CN';
+import { zh_TW } from './zh_TW';
 import { ja_JP } from './ja_JP';
 
 export type TTranslationMap = typeof en_GB.translations;
@@ -19,6 +20,7 @@ export const locales = [
     en_US,
     ru_RU,
     zh_CN,
+    zh_TW,
     fr_FR,
     ja_JP,
 ];
